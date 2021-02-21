f7f120e20dd986499d450485d6fbb6b87aa749ff
lzzr <bltko21@gmail.com>
2021-02-22 00:49:02 +1000

    main

--- JScourse/lesson1.js
+++ JScourse/lesson1.js
@@ -1,1 +1,11 @@
-console.log(123);
+var c = 12;
+var f = (9 / 5) * c + 32;
+
+console.log(f);
+
+var name = 'vasya';
+var admin = name;
+console.log(admin);
+
+var plus = (1000 + "108");
+console.log(plus);
